c7df1d788967f0b1c7bfa8efdd73840ffe027af6
alemcar <sandromuniz27@hotmail.com>
2024-07-29 12:14:36 -0400

    ajuste de formtação

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -5,9 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    'prettier',
+  ],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
